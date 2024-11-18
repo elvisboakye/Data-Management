1db9ff2886b076ab4dbf887c01fef35bf83ea19e
elvisboakye <ekayb2002@gmail.com>
2024-11-17 20:37:19 -0500

    Add files via upload

---
+++ lab11.sql
@@ -0,0 +1,21 @@
+SELECT title, due_date
+FROM assignments
+WHERE course_id LIKE 'COMP1234'
+
+SELECT min(due_date)
+FROM assignments
+
+SELECT max(due_date)
+FROM assignments
+
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08'
+
+SELECT title, due_date
+FROM assignments
+WHERE due_date like '2024-10%'
+
+SELECT max(due_date)
+FROM assignments
+WHERE status LIKE 'complete'
